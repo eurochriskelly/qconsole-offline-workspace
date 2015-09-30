e68c0e8b36bc30b045d012a593e12bee7a8e1011
Christopher Kelly <christopher.kelly@hinttech.com>
2015-09-30 20:39:27 +0200

    add watch function

--- bin/mlqcow.js
+++ bin/mlqcow.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
+'use strict';
+
+var argv = require('optimist').argv;
 var mlqcow = require('../lib/index.js');
 
+console.log(argv.option);
 console.log("Marklogic Query Console Offline Workspace");
+
+
+if (argv.watch) {
+    mlqcow.watch(function () {
+        console.log('Stopped watching');
+    });
+    return;
+}
+
 
 mlqcow.generate(function (e, result) {
     if (e) {
@@ -10,9 +23,11 @@ mlqcow.generate(function (e, result) {
         console.log(e);
         return;
     }
-    
     console.log('Workspace exported to file: ' + result);
 });
+
+
+
 
 process.on('error', function () {
     setTimeout(function () {
